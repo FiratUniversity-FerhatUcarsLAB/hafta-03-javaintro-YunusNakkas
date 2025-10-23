8ba9eab87444658f9a14c99a6a1c3f272fc3aa3e
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-23 16:44:13 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println(" Java'ya hoşgeldiniz");
+        System.out.println(" Bilgisayar Bilimlerine Hoş geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
