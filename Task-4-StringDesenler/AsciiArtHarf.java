8292122c93171cab004cd620c443971f4a1570a9
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-23 17:33:50 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("__   __");
+        System.out.println("\\ \\ / /");
+        System.out.println(" \\ V /");
+        System.out.println("  | |");
+        System.out.println("  | |");
+        System.out.println("  |_|");
+        
+
+    }
+}
