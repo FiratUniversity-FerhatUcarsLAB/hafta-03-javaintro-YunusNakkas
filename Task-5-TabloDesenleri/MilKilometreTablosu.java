b37dd2c3ab94dffa471cf78728f41867efc896f9
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-23 18:29:55 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        double s1=1;
+        double s2=5;
+        double s3=10;
+        double s4=20;
+        double s5=50;
+
+        double kilometre=1.609;
+
+        double s1sonuc=s1*kilometre;
+        double s2sonuc=s2*kilometre;
+        double s3sonuc=s3*kilometre;
+        double s4sonuc=s4*kilometre;
+        double s5sonuc=s5*kilometre;
+
+        System.out.println("1  kilometre: "+s1sonuc+" mildir. ");
+        System.out.println("5  kilometre: "+s2sonuc+" mildir. ");
+        System.out.println("10 kilometre: "+s3sonuc+" mildir. ");
+        System.out.println("20 kilometre: "+s4sonuc+" mildir. ");
+        System.out.println("50 kilometre: "+s5sonuc+" mildir. ");
+        
+        
+
+    }
+}
