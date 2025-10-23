879ddb1d49e065a850a4f63ef691e43dc8714fe8
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-23 18:38:48 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        int s1=1,s2=2,s3=3,s4=4,s5=5,s6=6,s7=7,s8=8,s9=9,s10=10;
+
+        int s1sonuc=s1*2,s2sonuc=s2*2,s3sonuc=s3*2,s4sonuc=s4*2,s5sonuc=s5*2,s6sonuc=s6*2,s7sonuc=s7*2,s8sonuc=s8*2,s9sonuc=s9*2,s10sonuc=s10*2;
+
+        System.out.println("=======  2'nin çarpım tablosu =====");
+        System.out.println("            2X1:  "+s1sonuc);
+        System.out.println("            2X2:  "+s2sonuc);
+        System.out.println("            2X3:  "+s3sonuc);
+        System.out.println("            2X4:  "+s4sonuc);
+        System.out.println("            2X5:  "+s5sonuc);
+        System.out.println("            2X6:  "+s6sonuc);
+        System.out.println("            2X7:  "+s7sonuc);
+        System.out.println("            2X8:  "+s8sonuc);
+        System.out.println("            2X9:  "+s9sonuc);
+        System.out.println("            2X10: "+s10sonuc);
+        System.out.println("=======  2'nin çarpım tablosu =====");
+    }
+}
