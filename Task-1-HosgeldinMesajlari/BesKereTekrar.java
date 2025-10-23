49ac7a14151bc9f0924253a093c6f8ccafcfd658
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-23 16:34:23 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+AD:Yunus Emre Nakkaş
+NUMARA:250542021
+
+  public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println("Java'ya hoşgeldiniz");
+        }
+    }
+}
